3cb79241c96e53ae69a92ef8359e3f64c3cbe780
Zac <ZacMaiBusiness@gmail.com>
2022-06-01 12:26:13 +1000

    Fixing terrain for AI navigation

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject GameOverScreen;
+    public static bool gameIsOver = false;
+   
+
+
+    void Start()
+    {
+        GameOverScreen.SetActive(false);
+    }
+    public void GameOver()
+    {
+        if (gameIsOver == false)
+        {
+            gameIsOver = true;
+            GameOverScreen.SetActive(true);
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+        }
+    }
+
+
+
+}
